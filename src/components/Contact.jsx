ae091793a4e16e4023aae61fcd71ecf7760edff3
sarangan16 <mailsarangan@gmail.com>
2025-07-30 11:11:25 +0200

    hero section image added, modification in hero section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="flex flex-col md:flex-row max-w-5xl mx-auto bg-white p-8 items-center md:items-start">
-      <div className="contact-hero-text md:w-1/2 pr-8 mb-8 md:mb-0">
+      <div className="contact-hero-text md:w-1/2 pr-8 mb-8 md:mb-0 flex items-center justify-center md:justify-start mt-6">
         <div className="w-full max-w-6xl px-4 sm:px-6">
           <div className="grid grid-cols-1">
             <div className="flex flex-col justify-center text-center md:text-left z-10 h-full">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,25 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <main class="dark:bg-gray-800 bg-white relative overflow-hidden h-screen">
-        <div class="bg-white dark:bg-[#e63946] flex relative z-20 items-center overflow-hidden">
-          <div class="container mx-auto px-6 flex relative py-16">
-            <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
-              <h1 class="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
+      <main className="dark:bg-gray-800 bg-white relative overflow-hidden">
+        <div className="bg-white dark:bg-[#e63946] flex items-center justify-between relative z-20 overflow-hidden">
+          <div className="container mx-auto px-6 flex py-16 w-full">
+            <div className="sm:w-2/3 lg:w-2/5 flex flex-col justify-center text-right">
+              <h1 className="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
                 Buy on
-                <span class="text-5xl sm:text-7xl">Time</span>
+                <span className="text-5xl sm:text-7xl">Time</span>
               </h1>
-              <p class="text-sm sm:text-base text-gray-700 dark:text-white">
+              <p className="text-sm sm:text-base text-gray-700 dark:text-white">
                 Dimension of reality that makes change possible and
                 understandable. An indefinite and homogeneous environment in
                 which natural events and human existence take place.
               </p>
             </div>
-            <div class="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
+
+            <div className="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
               <img
-                src="https://www.tailwind-kit.com/images/object/10.png"
-                class="max-w-xs md:max-w-sm m-auto"
+                src="/images/bear1.png"
+                className="max-w-xs md:max-w-sm m-auto w-full h-full object-cover"
               />
             </div>
           </div>

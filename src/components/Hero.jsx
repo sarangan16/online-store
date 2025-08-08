3eec6bf0820989e8dd6fee0e7d09fc71c68f9e83
sarangan16 <mailsarangan@gmail.com>
2025-08-08 13:01:14 +0200

    hero image changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div>
       <section
         className="relative py-20 md:py-24 text-gray-600 body-font overflow-x-hidden bg-cover bg-center bg-no-repeat min-h-[400px] shadow-lg"
-        style={{ backgroundImage: `url(/images/hero.jpg)` }}
+        style={{ backgroundImage: `url(/images/shopping.png)` }}
       >
         <div className="container mx-auto flex flex-col md:flex-row md:px-24 md:py-10 items-center min-h-full">
           <div className="lg:flex-grow flex flex-col items-center text-center md:items-start md:text-left w-full">
